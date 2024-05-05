936f38a67dae3bdad26fcb92b8c18bab4bc24d53
jjeris <johans.justs.eris@gmail.com>
2024-05-05 20:09:38 +0300

    Add basic function sigs

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello wowld uwu")
+# print("hello wowld uwu") #@Paddlebear was here UwU
+
+def main():
+    """
+    Main function.
+    """    
+    return None
+    
+def scrape_ipa(url: str):
+    """
+    Scrapes the IPA string from a provided Wikipedia URL
+
+    Args:
+        url (str): url to Wikipedia
+    """   
+    return url
+
