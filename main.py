363f480b4de7d38e6b2d884d5a4cce308c5f6d13
jjeris <johans.justs.eris@gmail.com>
2024-05-06 13:24:19 +0300

    Add basic input functions

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 # print("hello wowld uwu") #@Paddlebear was here UwU
+WIKIPEDIA_URL_EN = "https://en.wikipedia.org/wiki/"
+ACCEPTED_LANGUAGES = ["English", "Spanish", "Japanese", "French", "Ukrainian"]
 
 def main():
     """
     Main function.
-    """    
-    return None
+    """
+    user_input = get_input()
+    generate_proper_noun_wiki_url(user_input)
+    # scrape_ipa()
+
+def get_input():
+    proper_noun = str(input("Enter proper noun:"))
+    return proper_noun
+
     
-def scrape_ipa(url: str):
+def sanitize_input():
+    # TODO Agneses job :3
+    print("uwu")
+
+def generate_proper_noun_wiki_url(proper_noun):
+    print(f"{WIKIPEDIA_URL_EN}{proper_noun}")
+
+def scrape_ipa(url) -> str:
     """
     Scrapes the IPA string from a provided Wikipedia URL
 
@@ -15,3 +31,6 @@ def scrape_ipa(url: str):
     """   
     return url
 
+
+
+main()
